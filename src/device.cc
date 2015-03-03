55eb74a7a4098627908841f718a2371e5d9f0fa8
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2015-03-03 16:04:13 +0100

    Add Device binding glue

---
+++ src/device.cc
@@ -0,0 +1,178 @@
+#include "device.h"
+
+#include "events.h"
+#include "operation.h"
+#include "process.h"
+
+using v8::AccessorSignature;
+using v8::Array;
+using v8::DEFAULT;
+using v8::Exception;
+using v8::External;
+using v8::Function;
+using v8::FunctionCallbackInfo;
+using v8::FunctionTemplate;
+using v8::Handle;
+using v8::HandleScope;
+using v8::Integer;
+using v8::Isolate;
+using v8::Local;
+using v8::None;
+using v8::Number;
+using v8::Object;
+using v8::Persistent;
+using v8::PropertyCallbackInfo;
+using v8::String;
+using v8::Value;
+
+namespace frida {
+
+Persistent<Function> Device::constructor_;
+
+Device::Device(FridaDevice* handle)
+    : handle_(handle) {
+}
+
+Device::~Device() {
+  events_.Reset();
+  frida_unref(handle_);
+}
+
+void Device::Init(Handle<Object> exports) {
+  auto isolate = Isolate::GetCurrent();
+
+  auto tpl = FunctionTemplate::New(isolate, New);
+  tpl->SetClassName(String::NewFromUtf8(isolate, "Device"));
+  tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+  auto instance_tpl = tpl->InstanceTemplate();
+  auto data = Handle<Value>();
+  auto signature = AccessorSignature::New(isolate, tpl);
+  instance_tpl->SetAccessor(String::NewFromUtf8(isolate, "id"), GetId, 0,
+      data, DEFAULT, None, signature);
+  instance_tpl->SetAccessor(String::NewFromUtf8(isolate, "name"), GetName, 0,
+      data, DEFAULT, None, signature);
+  instance_tpl->SetAccessor(String::NewFromUtf8(isolate, "type"), GetType, 0,
+      data, DEFAULT, None, signature);
+
+  NODE_SET_PROTOTYPE_METHOD(tpl, "enumerateProcesses", EnumerateProcesses);
+
+  exports->Set(String::NewFromUtf8(isolate, "Device"),
+      tpl->GetFunction());
+
+  constructor_.Reset(isolate, tpl->GetFunction());
+}
+
+Local<Object> Device::Create(gpointer handle) {
+  auto isolate = Isolate::GetCurrent();
+
+  auto constructor = Local<Function>::New(isolate, constructor_);
+  const int argc = 1;
+  Local<Value> argv[argc] = { External::New(isolate, handle) };
+  return constructor->NewInstance(argc, argv);
+}
+
+void Device::New(const FunctionCallbackInfo<Value>& args) {
+  auto isolate = args.GetIsolate();
+  HandleScope scope(isolate);
+
+  if (args.IsConstructCall()) {
+    if (args.Length() != 1 || !args[0]->IsExternal()) {
+      isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate,
+          "Bad argument, expected raw handle")));
+      return;
+    }
+    auto wrapper = new Device(static_cast<FridaDevice*>(
+        Local<External>::Cast(args[0])->Value()));
+    auto obj = args.This();
+    wrapper->Wrap(obj);
+    obj->Set(String::NewFromUtf8(isolate, "events"),
+        Events::Create(g_object_ref(wrapper->handle_)));
+    args.GetReturnValue().Set(obj);
+  } else {
+    auto constructor = Local<Function>::New(isolate, constructor_);
+    args.GetReturnValue().Set(constructor->NewInstance(0, NULL));
+  }
+}
+
+void Device::GetId(Local<String> property,
+    const PropertyCallbackInfo<Value>& info) {
+  auto isolate = info.GetIsolate();
+  HandleScope scope(isolate);
+  auto handle = ObjectWrap::Unwrap<Device>(info.Holder())->handle_;
+
+  info.GetReturnValue().Set(Integer::New(isolate, frida_device_get_id(handle)));
+}
+
+void Device::GetName(Local<String> property,
+    const PropertyCallbackInfo<Value>& info) {
+  auto isolate = info.GetIsolate();
+  HandleScope scope(isolate);
+  auto handle = ObjectWrap::Unwrap<Device>(info.Holder())->handle_;
+
+  info.GetReturnValue().Set(
+      String::NewFromUtf8(isolate, frida_device_get_name(handle)));
+}
+
+void Device::GetType(Local<String> property,
+    const PropertyCallbackInfo<Value>& info) {
+  auto isolate = info.GetIsolate();
+  HandleScope scope(isolate);
+  auto handle = ObjectWrap::Unwrap<Device>(info.Holder())->handle_;
+
+  const gchar* type;
+  switch (frida_device_get_dtype(handle)) {
+    case FRIDA_DEVICE_TYPE_LOCAL:
+      type = "local";
+      break;
+    case FRIDA_DEVICE_TYPE_TETHER:
+      type = "tether";
+      break;
+    case FRIDA_DEVICE_TYPE_REMOTE:
+      type = "remote";
+      break;
+    default:
+      g_assert_not_reached();
+  }
+  info.GetReturnValue().Set(String::NewFromUtf8(isolate, type));
+}
+
+class EnumerateProcessesOperation : public Operation<FridaDevice> {
+ public:
+  void Begin() {
+    frida_device_enumerate_processes(handle_, OnReady, this);
+  }
+
+  void End(GAsyncResult* result, GError** error) {
+    processes_ = frida_device_enumerate_processes_finish(handle_, result, error);
+  }
+
+  Local<Value> Result(Isolate* isolate) {
+    auto size = frida_process_list_size(processes_);
+    auto processes = Array::New(isolate, size);
+    for (auto i = 0; i != size; i++) {
+      auto process = Process::Create(frida_process_list_get(processes_, i));
+      processes->Set(i, process);
+    }
+
+    g_object_unref(processes_);
+
+    return processes;
+  }
+
+  FridaProcessList* processes_;
+};
+
+void Device::EnumerateProcesses(const FunctionCallbackInfo<Value>& args) {
+  auto isolate = args.GetIsolate();
+  HandleScope scope(isolate);
+  auto obj = args.Holder();
+  auto wrapper = ObjectWrap::Unwrap<Device>(obj);
+
+  auto operation = new EnumerateProcessesOperation();
+  operation->Schedule(isolate, obj, wrapper->handle_);
+
+  args.GetReturnValue().Set(operation->GetPromise(isolate));
+}
+
+}
---
+++ src/device.h
@@ -0,0 +1,38 @@
+#ifndef FRIDANODE_DEVICE_H
+#define FRIDANODE_DEVICE_H
+
+#include <frida-core.h>
+#include <node_object_wrap.h>
+
+namespace frida {
+
+class Device : public node::ObjectWrap {
+ public:
+  static void Init(v8::Handle<v8::Object> exports);
+  static v8::Local<v8::Object> Create(gpointer handle);
+
+ private:
+  explicit Device(FridaDevice* handle);
+  ~Device();
+
+  static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+  static void GetId(v8::Local<v8::String> property,
+      const v8::PropertyCallbackInfo<v8::Value>& info);
+  static void GetName(v8::Local<v8::String> property,
+      const v8::PropertyCallbackInfo<v8::Value>& info);
+  static void GetType(v8::Local<v8::String> property,
+      const v8::PropertyCallbackInfo<v8::Value>& info);
+
+  static void EnumerateProcesses(
+      const v8::FunctionCallbackInfo<v8::Value>& args);
+
+  static v8::Persistent<v8::Function> constructor_;
+
+  FridaDevice* handle_;
+  v8::Persistent<v8::Object> events_;
+};
+
+}
+
+#endif
