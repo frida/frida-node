2b6d720295894f69953390d4576fd97711faffe0
Ole André Vadla Ravnås <oleavr@nowsecure.com>
2015-11-10 04:23:15 +0100

    Tweak the example a bit

--- examples/watch_devices.js
+++ examples/watch_devices.js
@@ -4,14 +4,14 @@ const co = require('co');
 const frida = require('..');
 
 const mgr = frida.getDeviceManager();
-mgr.events.listen('changed', function () {
-  console.log('changed!');
-});
 mgr.events.listen('added', function (device) {
   console.log('added!', device);
 });
 mgr.events.listen('removed', function (device) {
   console.log('removed!', device);
+});
+mgr.events.listen('changed', function () {
+  console.log('changed!');
 });
 
 co(function *() {
