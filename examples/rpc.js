a66c74aa73bacd50e5e9884fbaf16d82b1a472e1
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-06-02 15:59:33 +0200

    Add a tiny RPC example

---
+++ examples/rpc.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const co = require('co');
+const frida = require('..');
+
+const processName = process.argv[2];
+
+const source = `'use strict';
+
+rpc.exports = {
+  hello: function () {
+    return 'Hello';
+  },
+  failPlease: function () {
+    oops;
+  }
+};`;
+
+co(function *() {
+  const session = yield frida.attach(processName);
+  const script = yield session.createScript(source);
+
+  script.events.listen('message', message => {
+    console.log(message);
+  });
+
+  yield script.load();
+
+  const api = yield script.getExports();
+  console.log('api.load() =>', yield api.hello());
+
+  yield api.failPlease();
+})
+.catch(err => {
+  console.error(err);
+});
