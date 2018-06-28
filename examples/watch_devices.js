896ced323ad20f5d156c78579a88a47497054be5
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-06-28 15:26:18 +0200

    Improve the watch_devices example

--- examples/watch_devices.js
+++ examples/watch_devices.js
@@ -4,10 +4,10 @@ const frida = require('..');
 
 const deviceManager = frida.getDeviceManager();
 deviceManager.added.connect(device => {
-  console.log('added!', device.name);
+  console.log('added!', device);
 });
 deviceManager.removed.connect(device => {
-  console.log('removed!', device.name);
+  console.log('removed!', device);
 });
 deviceManager.changed.connect(() => {
   console.log('changed!');
@@ -15,7 +15,7 @@ deviceManager.changed.connect(() => {
 
 async function run() {
   const devices = await deviceManager.enumerateDevices();
-  console.log('enumerateDevices() =>', devices.map(device => device.name));
+  console.log('enumerateDevices() =>', devices);
 };
 
 run()
