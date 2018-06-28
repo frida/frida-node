2416cc5caabd9bac90b9ed406b79e0bd4d21518c
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-06-28 03:26:58 +0200

    Port interactive example to the new API

--- examples/interactive.js
+++ examples/interactive.js
@@ -1,51 +1,54 @@
 'use strict';
 
-const co = require('co');
 const frida = require('..');
 
-const source = 
-`recv(function onMessage(message) {
-  send({ name: "pong", payload: message });
-  recv(onMessage);
-});`;
+const source = `'use strict';
 
-const spawnExample = co.wrap(function *() {
-  const pid = yield frida.spawn(['/bin/cat', '/etc/resolv.conf']);
+recv(onMessage);
+
+function onMessage(message) {
+  send({ name: 'pong', payload: message });
+
+  recv(onMessage);
+}`;
+
+async function spawnExample() {
+  const pid = await frida.spawn(['/bin/cat', '/etc/resolv.conf']);
 
   console.log('spawned:', pid);
 
   // This is where you could attach (see below) and instrument APIs before you call resume()
-  yield frida.resume(pid);
+  await frida.resume(pid);
   console.log('resumed');
-});
+}
 
-const attachExample = co.wrap(function *() {
-  const session = yield frida.attach('cat');
+async function attachExample() {
+  const session = await frida.attach('cat');
   console.log('attached:', session);
 
-  const script = yield session.createScript(source);
+  const script = await session.createScript(source);
   console.log('script created:', script);
 
-  script.events.listen('message', (message, data) => {
+  script.message.connect((message, data) => {
     console.log('message from script:', message, data);
   });
 
-  yield script.load();
+  await script.load();
 
   console.log('script loaded');
   setInterval(() => {
     script.post({ name: 'ping' });
   }, 1000);
-});
+}
 
-const usbExample = co.wrap(function *() {
-  const device = yield frida.getUsbDevice(10000);
+async function usbExample() {
+  const device = await frida.getUsbDevice(10000);
 
   console.log('usb device:', device);
   // Now call spawn(), attach(), etc. on `device` just like the above calls on `frida`
-});
+}
 
 attachExample()
-.catch(error => {
-  console.log('error:', error.message);
-});
+  .catch(e => {
+    console.error(e);
+  });
