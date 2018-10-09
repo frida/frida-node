11d536f90a97fb930c81e583ea01bfca4b3301ec
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-10-09 03:10:14 +0200

    Add example showing how to enable debugger support

---
+++ examples/debugger.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const frida = require('..');
+
+const processName = process.argv[2];
+
+const source = `'use strict';
+
+setInterval(() => {
+  send(1337);
+}, 1000)`;
+
+async function main() {
+  const session = await frida.attach(processName);
+  await session.enableJit();
+  await session.enableDebugger();
+
+  const script = await session.createScript(source);
+  script.message.connect(message => {
+    console.log('[*] Message:', message);
+  });
+  await script.load();
+  console.log('[*] Script loaded');
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+  });
