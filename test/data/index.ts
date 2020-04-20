2bb08bfded61b0d1f3fd99416741a71c59127f18
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-04-20 23:22:46 +0000

    Use our own test binaries on Linux also
    So we can pick the appropriate one based on whether we're running on
    a 32- or 64-bit runtime. There's no need for our 32-bit builds to
    support injection into 64-bit targets.

--- test/data/index.ts
+++ test/data/index.ts
@@ -1,9 +1,11 @@
 export function targetProgram() {
     const platform = process.platform;
-    if (platform === "win32")
+    if (platform === "win32") {
         return "C:\\Windows\\notepad.exe";
-    else if (platform === "darwin")
+    } else if (platform === "darwin") {
         return __dirname + "/unixvictim-macos";
-    else
-        return "/bin/cat";
+    } else {
+        const arch = (process.arch === "x64") ? "x86_64" : "x86";
+        return __dirname + "/unixvictim-linux-" + arch;
+    }
 }
