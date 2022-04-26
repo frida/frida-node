3bb5a4a8d6d67521ffcfcf1a958a2baec4371362
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-04-26 08:58:18 +0200

    test: Use `/bin/cat` as target on most platforms

--- test/data/index.ts
+++ test/data/index.ts
@@ -5,13 +5,6 @@ export function targetProgram() {
     } else if (platform === "darwin") {
         return __dirname + "/unixvictim-macos";
     } else {
-        let fridaArch: string;
-        const nodeArch = process.arch;
-        if (nodeArch.startsWith("arm")) {
-            fridaArch = (nodeArch === "arm64") ? "arm64" : "armhf";
-        } else {
-            fridaArch = (nodeArch === "x64") ? "x86_64" : "x86";
-        }
-        return __dirname + "/unixvictim-linux-" + fridaArch;
+        return "/bin/cat";
     }
 }
