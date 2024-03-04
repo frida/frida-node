1c8b1c57e270a43e10e45ae47aa88e6c0e0e3a16
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-03-04 13:33:36 +0100

    native: Trim search path to the bare minimum

--- lib/native.ts
+++ lib/native.ts
@@ -4,15 +4,6 @@ export const binding = bindings({
     bindings: "frida_binding",
     try: [
         ["module_root", "build", "bindings"],
-        ["module_root", "build", "src", "bindings"],
-        ["module_root", "build", "Debug", "bindings"],
-        ["module_root", "build", "Release", "bindings"],
-        ["module_root", "out", "Debug", "bindings"],
-        ["module_root", "Debug", "bindings"],
-        ["module_root", "out", "Release", "bindings"],
-        ["module_root", "Release", "bindings"],
-        ["module_root", "build", "default", "bindings"],
-        ["module_root", "compiled", "version", "platform", "arch", "bindings"],
         [process.cwd(), "bindings"],
     ]
 });
