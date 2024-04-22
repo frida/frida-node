8efaee9133b40d310dc2d40cce606a3f168e5788
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-04-22 23:37:47 +0200

    meson: Compute version from VCS tags when building from git

--- scripts/detect-version.py
+++ scripts/detect-version.py
@@ -8,7 +8,13 @@ import sys
 def main(argv):
     pkg_file = Path(argv[1])
     pkg_data = json.loads(pkg_file.read_text(encoding="utf-8"))
-    print(pkg_data["version"])
+    version = pkg_data["version"]
+    if version == "0.0.0":
+        root_dir = Path(__file__).parent.parent.resolve()
+        sys.path.insert(0, str(root_dir))
+        from releng.frida_version import detect
+        version = detect(root_dir).name
+    print(version)
 
 
 if __name__ == "__main__":
