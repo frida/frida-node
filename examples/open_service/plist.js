c42f01324e23d8ca677c320abdadf59147e739c1
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-06-04 12:30:27 +0200

    examples: Fix the openService() plist example
    Co-authored-by: Håvard Sørbø <havard@hsorbo.no>

--- examples/open_service/plist.js
+++ examples/open_service/plist.js
@@ -5,8 +5,6 @@ async function main() {
   const device = await frida.getUsbDevice();
 
   const diag = await device.openService('plist:com.apple.mobile.diagnostics_relay');
-  await diag.request({ type: 'query', payload: { Request: 'RSDCheckin', ProtocolVersion: '2', Label: 'Frida' } });
-  await diag.request({ type: 'read' });
   await diag.request({ type: 'query', payload: { Request: 'Sleep', WaitForDisconnect: true } });
   await diag.request({ type: 'query', payload: { Request: 'Goodbye' } });
 }
